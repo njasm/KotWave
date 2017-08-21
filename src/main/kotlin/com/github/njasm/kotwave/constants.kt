eccab370eb12dbf1fbe558b459c59910341e69d0
Nelson J Morais <njmorais@proactive-hosters.com>
2017-08-21 02:24:17 +0100

    fix search and relase namespace...

--- src/main/kotlin/com/github/njasm/kotwave/constants.kt
+++ src/main/kotlin/com/github/njasm/kotwave/constants.kt
@@ -7,7 +7,7 @@ package com.github.njasm.kotwave
 // RFC 2616
 const val HTTP_HEADER_VALUE_DELIMITER : String = ","
 
-const val API_BASE_URL : String = "https://api.kotwave.com"
+const val API_BASE_URL : String = "https://api.soundcloud.com"
 const val API_CONNECT_PATH : String = "connect"
 const val API_TOKEN_PATH : String = "oauth2/token"
 
